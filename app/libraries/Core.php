a025ec65bfbb7cd3310f0e3754d19fe4057328ff
Catalin Rizea <catalinrizea1@gmail.com>
2018-11-06 13:33:58 +0200

    clean up router

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -10,52 +10,33 @@ class Core {
   protected
     $currentController = 'Pages',
     $currentMethod = 'index',
-    $params = [];
+    $params = [],
+    $url;
 
   public function __construct() {
-    // print_r($this->getUrl());
-    $url = $this->getUrl();
-
-    $this->setCurrentController($url);
-    // unset 0 index
-    unset($url[0]);
-    
-    $this->setCurrentMethod($url);
-    // unset 1 index
-    unset($url[1]);
-
-    $this->setParams($url);
+    $this->url = $this->getUrl();
+    $this->setCurrentController()->setCurrentMethod()->setParams();
   }
 
-  private function setParams($url) {
-    $this->params = $url ? array_values($url) : [];
-    
-    // call a callback with this array of params
+  private function setParams() {
+    $this->params = $this->url[2] ? [$this->url[2]] : [];
     call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
-
-    return $this;
   }
 
-  private function setCurrentMethod($url) {
-    // check for the second part of the url
-    if (isset($url[1]) && method_exists($this->currentController, $url[1])) {
-      $this->currentMethod = $url[1];  
+  private function setCurrentMethod() {
+    if (isset($this->url[1]) && method_exists($this->currentController, $this->url[1])) {
+      $this->currentMethod = $this->url[1];  
     }
 
     return $this;
   }
 
-  private function setCurrentController($url) {
-    // look in controller for first value
-    if (isset($url[0]) && file_exists('../app/controllers/'.ucwords($url[0]).'.php')) {
-      // if exists then set as controller
-      $this->currentController = ucwords($url[0]);
+  private function setCurrentController() {
+    if (isset($this->url[0]) && file_exists('../app/controllers/'.ucwords($this->url[0]).'.php')) {
+      $this->currentController = ucwords($this->url[0]);
     }
 
-    // require the controller
     require_once '../app/controllers/'.$this->currentController.'.php';
-
-    // instantiate controller class
     $this->currentController = new $this->currentController;
 
     return $this;
